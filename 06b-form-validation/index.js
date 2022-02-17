ec8f5d6a89263af49b3bfd17bd910b9530060339
xunne899 <yixun89@live.com>
2022-02-17 10:05:37 +0000

    practice

--- 06b-form-validation/index.js
+++ 06b-form-validation/index.js
@@ -27,7 +27,6 @@ app.post('/login', function(req,res){
     // 2. password must not be empty and contains at three characters
     let email = req.body.email;
     let password = req.body.password;
-
     let hasError = false;
 
     // if the email is null, undefined or empty string, then we report the error
@@ -50,10 +49,70 @@ app.post('/login', function(req,res){
     if (hasError) {
         res.sendStatus(406)
     } else {
-        res.send("Success");
+        res.send("Success")
     }
     
     
+})
+    
+app.get('/lost&found', function(req,res){
+    res.render('lost&found')
+})
+
+app.post('/lost&found', function(req,res){
+    // valdiation rules:
+    // 1. email must not be empty and contains at least one @
+    // 2. password must not be empty and contains at three characters
+    let item = req.body.item;
+    let email = req.body.email;
+    let transport = req.body.transport
+    let form = [];
+    if (req.body.form) {
+        // if true, it means req.body.items is an array
+        if (Array.isArray(req.body.form)) {
+            form = req.body.form;
+        } else {
+            // if false, then it means req.body.items is a string
+            form = [ req.body.form ];
+            // Suppose that req.body.items is 'durian', then:
+            // fruits = [ req.body.items ]
+            // => fruits = [ 'durian' ]
+        }
+    }
+console.log(form)
+    let hasError = false;
+
+
+    if (!transport){
+        hasError = true;
+    }
+    // if the email is null, undefined or empty string, then we report the error
+    if (!item) {
+        hasError = true;
+    } 
+    // make sure the password is not undefiend or empty string first,
+    // then make it has three at least three characters
+    if (item && item.length < 3 || item.length > 200) {
+        hasError = true;
+    }
+    if (!email) {
+        hasError = true;
+    } 
+    // make sure email is not undefined or empty string
+    // then make sure it has the @ character
+    if ( email && !email.includes('@') && !email.includes('.')) {
+        hasError = true;
+    }
+ 
+    if (form && form.length>3 || form.length<1){
+        hasError = true;
+    }
+
+    if (hasError) {
+        res.sendStatus(406)
+    } else {
+        res.send("Success");
+    }
 })
 
 app.listen(3000, function(){
